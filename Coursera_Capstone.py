5656de8d56d4eee15f4c65e8da0c429c076420d9
Rena Spyropoulou <diangni.siach@gmail.com>
2020-03-30 22:53:37 +0300

    week 1 assignment

---
+++ Coursera_Capstone.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1> This notebook will be mainly used for the Capstone project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!' )
+
+
+# In[ ]:
+
+
+
+
